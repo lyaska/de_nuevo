aa1ddfa5d09a37b4c6ac535373575a55496095f0
Anastasia <alexeeva.nastia.1994@gmail.com>
2020-03-23 10:50:33 +0500

    Добавила регистрацию поль-ей(?)

--- src/main/java/com/sed/controllers/MainController.java
+++ src/main/java/com/sed/controllers/MainController.java
@@ -1,4 +1,4 @@
-package com.sed;
+package com.sed.controllers;
 
 import com.sed.domain.Task;
 import com.sed.repos.TaskRepo;
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.Map;
 
 @Controller
-public class GreetingController {
+public class MainController {
     @Autowired
     private TaskRepo taskRepo;
 
---
+++ src/main/java/com/sed/domain/Role.java
@@ -0,0 +1,5 @@
+package com.sed.domain;
+
+public enum Role {
+    USER;
+}
