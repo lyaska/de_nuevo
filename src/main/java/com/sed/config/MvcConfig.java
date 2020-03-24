116acacb9747d051fe86a2f82ef3d521b54873f4
Anastasia <alexeeva.nastia.1994@gmail.com>
2020-03-24 18:48:59 +0500

    попытка во фронт

--- src/main/java/com/sed/config/MvcConfig.java
+++ src/main/java/com/sed/config/MvcConfig.java
@@ -1,6 +1,7 @@
 package com.sed.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -9,6 +10,12 @@ public class MvcConfig implements WebMvcConfigurer {
 
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/login").setViewName("login");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("classpath:/static/");
     }
 
 }
